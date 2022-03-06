daee0e6c3110a3de6adeb5afb5105962230d0b93
moyan <moyan@DESKTOP-2K0DVI3>
2022-03-06 12:24:11 +0100

    Documentacion javadoc agregada

--- TareaEDD4/src/CCuenta.java
+++ TareaEDD4/src/CCuenta.java
@@ -1,46 +1,33 @@
+/**
+ * Clase encargada de crear una cuenta y todas sus operaciones
+ * 
+ * @author moyan
+ * @version 1.0
+ * @since 1.0
+ *
+ */
 public class CCuenta {
 
+	// Variables de clase - nombre, cuenta, saldo y tipoInteres
 	private String nombre;
     private String cuenta;
     private double saldo;
     private double tipoInteres;
 
-    public String getNombre() {
-		return nombre;
-	}
-
-	public void setNombre(String nombre) {
-		this.nombre = nombre;
-	}
-
-	public String getCuenta() {
-		return cuenta;
-	}
-
-	public void setCuenta(String cuenta) {
-		this.cuenta = cuenta;
-	}
-
-	public double getSaldo() {
-		return saldo;
-	}
-
-	public void setSaldo(double saldo) {
-		this.saldo = saldo;
-	}
-
-	public double getTipoInteres() {
-		return tipoInteres;
-	}
-
-	public void setTipoInteres(double tipoInteres) {
-		this.tipoInteres = tipoInteres;
-	}
-
+	/**
+	 * Constructor vacio
+	 */
     public CCuenta()
     {
     }
 
+    /**
+     * Constructor de la clase 
+     * @param nom nombre
+     * @param cue cuenta
+     * @param sal saldo
+     * @param tipo tipo de cuenta (deprecated)
+     */
     public CCuenta(String nom, String cue, double sal, double tipo)
     {
         nombre =nom;
@@ -48,11 +35,20 @@ public class CCuenta {
         saldo=sal;
     }
 
+    /**
+     * Estado (saldo) de la cuenta
+     * @return saldo
+     */
     public double estado()
     {
         return saldo;
     }
 
+    /**
+     * Metodo para ingresar dinero en la cuenta
+     * @param cantidad - cantidad a ingresar
+     * @throws Exception
+     */
     public void ingresar(double cantidad) throws Exception
     {
         if (cantidad<0)
@@ -60,6 +56,11 @@ public class CCuenta {
         saldo = saldo + cantidad;
     }
 
+    /**
+     * Metodo para retirar dinero de la cuenta
+     * @param cantidad - cantidad a retirar
+     * @throws Exception
+     */
     public void retirar(double cantidad) throws Exception
     {
         if (cantidad <= 0)
@@ -68,4 +69,69 @@ public class CCuenta {
             throw new Exception ("No se hay suficiente saldo");
         saldo = saldo - cantidad;
     }
+    
+    /**
+     * Getter nombre
+     * @return nombre nombre
+     * 
+     */
+    public String getNombre() {
+		return nombre;
+	}
+
+    /**
+     * Setter nombre
+     * @param nombre nombre
+     */
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	
+    /**
+     * Getter cuenta
+     * @return cuenta cuenta
+     */
+	public String getCuenta() {
+		return cuenta;
+	}
+
+	/**
+	 * Setter cuenta
+	 * @param cuenta cuenta
+	 */
+	public void setCuenta(String cuenta) {
+		this.cuenta = cuenta;
+	}
+
+    /**
+     * Getter saldo
+     * @return saldo saldo
+     */
+	public double getSaldo() {
+		return saldo;
+	}
+
+	/**
+	 * Setter saldo
+	 * @param saldo saldo
+	 */
+	public void setSaldo(double saldo) {
+		this.saldo = saldo;
+	}
+
+    /**
+     * Getter tipo interes
+     * @return tipoInteres tipo Interes
+     */
+	public double getTipoInteres() {
+		return tipoInteres;
+	}
+
+	/**
+	 * Setter tipo Interes
+	 * @param tipoInteres tipo interes
+	 */
+	public void setTipoInteres(double tipoInteres) {
+		this.tipoInteres = tipoInteres;
+	}
 }
--- TareaEDD4/src/Main.java
+++ TareaEDD4/src/Main.java
@@ -1,19 +1,36 @@
+/**
+ * Clase principal, encargada de realizar las operaciones de la cuenta
+ * 
+ * @author moyano
+ * @version 1.0
+ * @since 1.0
+ */
 public class Main {
 
     public static void main(String[] args) {
+    	// Cuenta en la que operar
         CCuenta cuenta1;
+        // saldo actual
         double saldoActual;
-        float saldoAct = 12;
-        cuenta1 = new CCuenta("Antonio LÃ³pez","1000-2365-85-1230456789",2500,0);
+        cuenta1 = new CCuenta("Antonio López","1000-2365-85-1230456789",2500,0);
         saldoActual = cuenta1.estado();
         System.out.println("El saldo actual es"+ saldoActual );
 
-        operativa_cuenta(cuenta1, saldoAct);
+        /**
+         * Operacion de la cuenta
+         */
+        operativa_cuenta(cuenta1, saldoActual);
     }
 
-	private static void operativa_cuenta(CCuenta cuenta1, float cantidad) {
+    /**
+     * Operativa cuenta se encarga de retirar una cantidad introducida por parametro
+     * y de ingresar una cantidad fija (695)
+     * @param cuenta1 cuenta en la que operar
+     * @param saldoActual cantidad a operar
+     */
+	private static void operativa_cuenta(CCuenta cuenta1, double saldoActual) {
 		try {
-            cuenta1.retirar(cantidad);
+            cuenta1.retirar(saldoActual);
         } catch (Exception e) {
             System.out.print("Fallo al retirar");
         }
